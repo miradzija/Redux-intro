07e73ad49bf6cb5034a883cf9a5bbde68e58d51f
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-03 23:29:17 +0200

    redux package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store-v1";
@@ -10,7 +11,9 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
