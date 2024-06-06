269d0f3b2472aa0e6e84ce725d9d1d34cf1fc7f9
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-06 23:30:43 +0200

    trunk

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -20,9 +20,8 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    // dispatch(deposit(depositAmount, currency));
-    dispatch(deposit(depositAmount));
-    setDepositAmount("");
+    dispatch(deposit(depositAmount, currency));
+
     setCurrency("USD");
   }
 
