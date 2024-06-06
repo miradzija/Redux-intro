03f52efafd87c3cc2847f45efc7f653632528441
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-06 23:21:38 +0200

    account slice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -14,23 +14,35 @@ const accountSlice = createSlice({
     deposit(state, action) {
       state.balance += action.payload;
     },
-    witdrow(state, action) {
+    withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan === 0) return;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
 
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+      reducer(state, action) {
+        if (state.loan === 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
-      state.balance -= state.loan;
     },
   },
 });
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
 
 /*
 export default function accountReducer(state = initialState, action) {
