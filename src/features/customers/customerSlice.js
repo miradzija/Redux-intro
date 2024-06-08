39474717fbe29ceb557182019ba68af63c7c0f7a
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-08 23:13:20 +0200

    finish

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,9 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initiialStateCustomer = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.action;
+
+export default customerSlice.reducer;
+
+/*
 export default function customerReducer(state = initiialStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -32,3 +65,4 @@ export function createCustomer(fullName, nationalID) {
 export function updateName(fullName) {
   return { type: "account/updateName", payload: fullName };
 }
+*/
